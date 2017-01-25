03f679918b2001478b0066bc8ce9513a7bbe9509
Stuart Baker <stuartbaker@Stuarts-MacBook-Pro.local>
2017-01-24 21:33:42 -0500

    Changing parsing so that results would not be affected by commas in strings. Finished data model for BMG format

---
+++ Royalty_UI/Royalty_UI/ASCAP_Model_Domestic.swift
@@ -0,0 +1,9 @@
+//
+//  ASCAP_Model_Domestic.swift
+//  Royalty_UI
+//
+//  Created by Stuart Baker on 1/18/17.
+//  Copyright © 2017 Stuart Baker. All rights reserved.
+//
+
+import Foundation
---
+++ Royalty_UI/Royalty_UI/BMG_Model.swift
@@ -0,0 +1,9 @@
+//
+//  BMG_Model.swift
+//  Royalty_UI
+//
+//  Created by Stuart Baker on 1/19/17.
+//  Copyright © 2017 Stuart Baker. All rights reserved.
+//
+
+import Foundation
